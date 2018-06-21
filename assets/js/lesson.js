f6e75ff832efce54060a0fc186928f4adc4fd1cf
Remi Rampin <remirampin@gmail.com>
2018-06-20 20:00:57 -0400

    Remove merge conflict markers from lesson.js
    Amends 1ff43804006185d9f60a086cf87b33c1e7514edb.

--- assets/js/lesson.js
+++ assets/js/lesson.js
@@ -3,21 +3,16 @@ $("table").addClass("table table-striped");
 
 
 // Handle foldable challenges and solutions (on click and at start).
-=======
-    $(".solution").click(function(event) {
-	var trigger = $(event.target).has(".fold-unfold").size() > 0
-            || $(event.target).filter(".fold-unfold").size() > 0;
-	if (trigger) {
-            $(">*:not(h2)", this).toggle(400);
-            $(">h2>span.fold-unfold", this).toggleClass("glyphicon-collapse-down glyphicon-collapse-up");
-            event.stopPropagation();
-	}
-    });
-    <<<<<<< HEAD
-$(".discussion,.solution").each(function() {
-=======
+$(".solution").click(function(event) {
+    var trigger = $(event.target).has(".fold-unfold").size() > 0
+               || $(event.target).filter(".fold-unfold").size() > 0;
+    if (trigger) {
+        $(">*:not(h2)", this).toggle(400);
+        $(">h2>span.fold-unfold", this).toggleClass("glyphicon-collapse-down glyphicon-collapse-up");
+        event.stopPropagation();
+    }
+});
 $(".solution").each(function() {
->>>>>>> e9acce29f4e236548676aaa41bd8864eb50c18cc
     $(">*:not(h2)", this).toggle();
     var h2 = $("h2:first", this);
     h2.append("<span class='fold-unfold glyphicon glyphicon-collapse-down'></span>");
